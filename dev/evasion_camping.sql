e839b51ceaf150e01ad42f70eac480770b8f6858
YvanBlanchette <yvanblanchette@gmail.com>
2024-07-22 01:29:33 -0400

    added the database script in the dev folder

--- dev/evasion_camping.sql
+++ dev/evasion_camping.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : lun. 22 juil. 2024 à 05:26
+-- Généré le : lun. 22 juil. 2024 à 05:28
 -- Version du serveur : 8.0.36
 -- Version de PHP : 7.4.33
 
